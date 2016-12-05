04776f44a8042bf79cd6e5f6ca9cf1bde61d6ef0
yuokada <callistoiv+git@gmail.com>
2016-12-06 01:13:56 +0900

    Add comment

--- practice/haskell-practice/chapter04/uniq.hs
+++ practice/haskell-practice/chapter04/uniq.hs
@@ -1,3 +1,10 @@
+-- $ runghc chapter04/uniq.hs < chapter04/practice_uniq.txt 
+-- AK	Alaska
+-- AL	Alabama
+-- AR	Arkansas
+-- AZ	Arizona
+-- CA	Califolnia
+--
 import           Data.List
 
 main = do cs <- getContents
