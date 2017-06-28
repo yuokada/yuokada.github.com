1f3432fbe03e7c29459972ef8089321442352178
yuokada <callistoiv+git@gmail.com>
2017-06-29 08:23:41 +0900

    Add java sample codes

---
+++ practice/java/guava-sample/src/main/java/GuavaCach.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
+
+/**
+ * Created by yukihirookada on 2017/06/29.
+ */
+public class GuavaCach {
+
+  public static void main(String[] args) {
+    StringJoiner joiner = new StringJoiner(" , ", "{", "]");
+    joiner.add("d").add("e").add("f").toString();
+    System.out.println(joiner.toString());
+
+//    List<String> ls = new ArrayList<String>("f", "g");
+    List<String> ls1 = Arrays.asList("foo", "bar", "baz");
+
+    String[] ls = new String[]{"foo", "bar", "baz"};
+    List<String> ls2 = Arrays.asList(ls);
+
+    System.out.println(ls.hashCode());
+    System.out.println(ls2.toString());
+
+  }
+}
