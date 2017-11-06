81b8167725d516cd310c81b573f8386a373b9bdc
yuokada <callistoiv+git@gmail.com>
2017-11-07 01:25:10 +0900

    sbt scalafmt

--- practice/scala1/build.sbt
+++ practice/scala1/build.sbt
@@ -5,3 +5,5 @@ version := "0.1"
 scalaVersion := "2.12.4"
 
 scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked", "-Xlint")
+
+// scalafmtConfig in ThisBuild := Some(file(".scalafmt.conf"))
--- practice/scala1/src/main/scala/HelloWorld.scala
+++ practice/scala1/src/main/scala/HelloWorld.scala
@@ -2,10 +2,9 @@ object HelloWorld {
 
   def sum(n: Int): Int = {
     def rec_sum(nn: Int): Int = {
-      if (nn <= 0) {
-        return 0
-      } else {
-        return nn + rec_sum(nn - 1)
+      nn match {
+        case nn if nn <= 0 => return 0
+        case _             => return nn + rec_sum(nn - 1)
       }
     }
 
@@ -14,7 +13,7 @@ object HelloWorld {
 
   def main(args: Array[String]): Unit = {
     println("Hello, World!")
-    val ls = List[Int](1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val ls = List[Int](1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
     for (j <- ls) {
       //      println(j)
       println(j, sum(j))
