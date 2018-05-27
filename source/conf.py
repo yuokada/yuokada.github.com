83bf1b7414b07fe5deaac2bcc58b3b28d4293e18
yuokada <callistoiv+git@gmail.com>
2018-05-27 12:18:32 +0900

    Update for python3.6

--- source/conf.py
+++ source/conf.py
@@ -27,7 +27,8 @@ import sphinx_bootstrap_theme
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinxtogithub']
+# extensions = ['sphinx.ext.autodoc', 'sphinxtogithub']
+extensions = ['sphinx.ext.autodoc', ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
